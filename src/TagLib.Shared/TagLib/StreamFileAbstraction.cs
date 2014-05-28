76d892f91975dc6cbf1934ff0ec92b209b6f7f5a
Tim Heuer <tim@timheuer.com>
2014-05-28 08:01:35 -0700

    Added LGPL license details

--- src/TagLib.Shared/TagLib/StreamFileAbstraction.cs
+++ src/TagLib.Shared/TagLib/StreamFileAbstraction.cs
@@ -1,4 +1,24 @@
-﻿using System.IO;
+﻿//
+// StreamFileAbstraction.cs:
+//
+// Author:
+//   Tim Heuer (tim@timheuer.com)
+//
+// Copyright (C) 2014 Tim Heuer
+//
+// This library is free software; you can redistribute it and/or modify
+// it  under the terms of the The MIT License (MIT).
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See The MIT License (MIT) for more details.
+//
+// You should have received a copy of The MIT License (MIT)
+// along with this library.
+//
+
+using System.IO;
 
 namespace TagLib
 {
