3df3b1fc3010132a23ac447503f6592d32b745ee
Tim Heuer <tim@timheuer.com>
2015-10-29 17:13:15 -0700

    Bump version number

--- src/TagLib.Shared/AssemblyInfo.cs
+++ src/TagLib.Shared/AssemblyInfo.cs
@@ -26,10 +26,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly:AssemblyVersion("2.1.0.0")]
+[assembly:AssemblyVersion("2.2.0.0")]
 [assembly:AssemblyTitle ("TagLib#")]
 [assembly:AssemblyDescription ("A library for reading and writing audio metatags.")]
-[assembly:AssemblyCopyright ("Copyright (c) 2006-2007 Brian Nickel.  Copyright (c) 2009-2010 Other contributors")]
+[assembly:AssemblyCopyright ("Copyright (c) 2006-2007 Brian Nickel.  Copyright (c) 2009-2015 Other contributors")]
 [assembly:AssemblyCompany ("")]
 [assembly:AssemblyDelaySign(false)]
 [assembly:CLSCompliant(false)]
